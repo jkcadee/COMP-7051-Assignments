a451ed274364d90abf9efcd7ac3920c16a350799
luminaryFlowers <apthomlinson@gmail.com>
2022-09-28 14:57:15 -0700

    ball bounces while keeping momentum before endlessly bouncing vertically on the board

--- Assignment-1/Assets/Scripts/BallBehaviour.cs
+++ Assignment-1/Assets/Scripts/BallBehaviour.cs
@@ -7,16 +7,24 @@ public class BallBehaviour : MonoBehaviour
 {
 
     Rigidbody rb;
+    public float speed = 30f;
+
+    void StartBallMovement()
+    {
+        Vector3 v3 = new Vector3(speed, 0, speed);
+        rb.AddForce(v3, ForceMode.VelocityChange);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        StartBallMovement();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.velocity = rb.velocity.normalized * speed;
     }
 }
