099b5e652652df0f8a7c2cbde144c5cd56d0bef8
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2022-11-30 21:17:43 -0800

    makes new enemy

--- Assignment-2/Assets/Scripts/Ball/EnemyBallHealth.cs
+++ Assignment-2/Assets/Scripts/Ball/EnemyBallHealth.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBallHealth : MonoBehaviour
 {
     int health = 3;
+    float purgatoryTimeout = 0;
+    float y;
+    public GameObject enemyPrefab;
 
     private void OnCollisionEnter(Collision other)
     {
@@ -13,8 +17,27 @@ public class EnemyBallHealth : MonoBehaviour
             health--;
             if(health < 1)
             {
-                Destroy(gameObject);
+                purgatoryTimeout = 5;
+                y = transform.position.y;
+                GetComponent<NavMeshAgent>().enabled = false;
+                transform.position = new Vector3(1000, -100, 1000);
             }
+        }
+    }
+
+    private void Update()
+    {
+        if(purgatoryTimeout > 0)
+        {
+            purgatoryTimeout -= Time.deltaTime;
+        }
+        else if (purgatoryTimeout < 0)
+        {
+            purgatoryTimeout = 0;
+            int x = Random.Range(-2, 2);
+            int z = Random.Range(-2, 2);
+            GameObject enemy = Instantiate(enemyPrefab, new Vector3(x, y, z), Quaternion.identity);
+            Destroy(gameObject);
         }
     }
 }
